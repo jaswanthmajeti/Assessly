af1de9e8797c23342946aea2da73f5cd57cf0b39
MAJETI JASWANTH SAI <jaswanthsaimajeti@gmail.com>
2026-06-18 17:01:55 +0530

    feat: add Assessly landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,20 @@ import { useUser} from "@clerk/react";
 import { Navigate, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import ProblemsPage from "./pages/ProblemsPage";
+import DashboardPage from "./pages/DashboardPage";
 import { Toaster } from "react-hot-toast";
 
 function App() {
-  const { isSignedIn } = useUser();
+  const { isSignedIn , isLoaded} = useUser();
+
+  // this will get rid of the flickering effect
+  if (!isLoaded) return null;
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={ !isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
       </Routes>
       <Toaster toastOptions={{ duration: 3000 }} />
